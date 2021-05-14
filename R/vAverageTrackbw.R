41e14dfd464b5111feb5b74b6f26b3b80329ca0a
vincent.loubiere <vincent.loubiere@clip-c1-17.cbe.vbc.ac.at>
2021-05-14 10:52:07 +0200

    aggregate tracks now supports bedgraph

--- R/vAverageTrackbw.R
+++ R/vAverageTrackbw.R
@@ -14,43 +14,43 @@
 #' @param ylim ylim for plotting. default= range(data)
 #' @param col Colors to use. If specified, should match length(tracks)*length(unique(set_IDs)). default= NULL (redirects to rainbow)
 #' @param axes Should the x axis be plotted? Default to T
-#' @param legend Should the legen be plotted? default to T
+#' @param legend Should the legend be plotted? default to T
 #' @examples 
 #' Unstranded 
 #' STARR <- fread("/groups/stark/vloubiere/projects/gw_STARRSeq_bernardo/db/peaks/DSCP_600bp_gw_cut_merged.peaks.txt", select = c(1,2))
 #' STARR <- STARR[, .(seqnames= V1, start= V2, end= V2)]
-#' obj1 <- vl_average_bw_track(bed= STARR,
-#'                             set_IDs = c(rep("High", 2000), rep("Low", 2781)),
-#'                             #' extend = c(-5000, 5000),
-#'                             stranded = F,
-#'                             tracks= "/groups/stark/vloubiere/projects/gw_STARRSeq_bernardo/db/bw/DSCP_600bp_gw_cut_merged.bw", 
-#'                             col= c("lightgrey", "red"), 
-#'                             names = c("STARR-Seq"))
+#' obj1 <- vl_average_track(bed= STARR,
+#'                          tracks= "/groups/stark/vloubiere/projects/gw_STARRSeq_bernardo/db/bw/DSCP_600bp_gw_cut_merged.bw", 
+#'                          set_IDs = c(rep("High", 2000), rep("Low", 2781)),
+#'                          #' extend = c(-5000, 5000),
+#'                          stranded = F,
+#'                          col= c("lightgrey", "red"), 
+#'                          names = c("STARR-Seq"))
 #'
 #' Stranded 
 #' prom <- readRDS("/groups/stark/hendy/Projects/Chromatin_remodeler_specificity/Analyses/Rdata/unique_proms_merged.df.RDS")
 #' prom <- as.data.table(GRanges(unique(prom$Flybase.TSS)))
-#' obj2 <- vl_average_bw_track(bed= prom,
-#'                             extend = c(-5000, 5000),
-#'                             stranded = T,
-#'                             tracks= "/groups/stark/vloubiere/projects/available_data_dm3/db/bw/GSE81795_H3K4me3_rep1_uniq.bw")
-#' @return An object that can be used with the vl_average_bw_track_plot_only() function.
+#' obj2 <- vl_average_track(bed= prom,
+#' tracks= "/groups/stark/vloubiere/projects/available_data_dm3/db/bw/GSE81795_H3K4me3_rep1_uniq.bw",
+#' extend = c(-5000, 5000),
+#' stranded = T)
+#' @return An object that can be used with the vl_average_track_plot_only() function.
 #' @export
 
-vl_average_bw_track <- function(bed,
-                                set_IDs= NULL,
-                                tracks,
-                                extend= c(-5000, 5000),
-                                stranded= F,
-                                nbins= 501, 
-                                names= NULL,
-                                plot= T,
-                                xlab= "genomic distance",
-                                ylab= "Enrichment",
-                                ylim= NULL,
-                                col= NULL, 
-                                axes= T,
-                                legend= T)
+vl_average_track <- function(bed,
+                             tracks,
+                             set_IDs= NULL,
+                             extend= c(-5000, 5000),
+                             stranded= F,
+                             nbins= 501, 
+                             names= NULL,
+                             plot= T,
+                             xlab= "genomic distance",
+                             ylab= "Enrichment",
+                             ylim= NULL,
+                             col= NULL, 
+                             axes= T,
+                             legend= T)
 {
   if(class(bed)[1]=="GRanges")
     bed <- data.table::as.data.table(bed)
@@ -63,7 +63,9 @@ vl_average_bw_track <- function(bed,
   if(stranded & !("strand" %in% colnames(bed)))
     stop("stranded= T but no 'strand' column in bed file")
   if(any(!file.exists(tracks)))
-    stop("some bw files could not be found! full paths prodvided?")
+    stop("some track files could not be found! full paths prodvided?")
+  if(any(!grepl(".bw$|.bedgraph$|.bedgraph.gz$|.bg$|.bg.gz$", tracks)))
+    stop("some track files formats are not supported. Supported extensions: .bw, .bedgraph, .bedgraph.gz, .bg, .bg.gz")
   if(is.null(names))
     names <- gsub(".bw$", "", basename(tracks))
   if(length(names) != length(tracks))
@@ -96,7 +98,12 @@ vl_average_bw_track <- function(bed,
   sel <- rtracklayer::BigWigSelection(sel, "score")
   q <- parallel::mclapply(tracks, function(x)
   {
-    .c <- data.table::as.data.table(rtracklayer::import.bw(x, selection= sel))
+    if(grepl(".bw$", x))
+      .c <- data.table::as.data.table(rtracklayer::import(x, selection= sel))
+    if(grepl(".bedgraph$|.bedgraph.gz$|.bg$|.bg.gz$", x))
+      .c <- data.table::fread(x, 
+                              fill= T, 
+                              col.names = c("seqnames", "start", "end", "score"))
     data.table::setkeyv(.c, c("seqnames", "start", "end"))
     res <- data.table::foverlaps(.c, bins, nomatch = 0)
     res <- res[, .(score= max(abs(score), na.rm= T)), .(set_ID, region_ID, bin_ID)]
@@ -156,9 +163,9 @@ vl_average_bw_track <- function(bed,
 
 #' bw Average tracks plot only
 #'
-#' Plots average tracks from a vl_average_bw_track output object
+#' Plots average tracks from a vl_average_track output object
 #'
-#' @param obj An object returned by the vl_average_bw_track() function. The object can easily be modified to change colors and so on...
+#' @param obj An object returned by the vl_average_track() function. The object can easily be modified to change colors and so on...
 #' @param ylim ylim for plotting. default= range(data)
 #' @param xlab X label. default= "genomic distance"
 #' @param ylab Y labels. default= "Enrichment"
@@ -168,32 +175,32 @@ vl_average_bw_track <- function(bed,
 #' Unstranded 
 #' STARR <- fread("/groups/stark/vloubiere/projects/gw_STARRSeq_bernardo/db/peaks/DSCP_600bp_gw_cut_merged.peaks.txt", select = c(1,2))
 #' STARR <- STARR[, .(seqnames= V1, start= V2, end= V2)]
-#' obj1 <- vl_average_bw_track(bed= STARR,
-#'                             set_IDs = c(rep("High", 2000), rep("Low", 2781)),
-#'                             #' extend = c(-5000, 5000),
-#'                             stranded = F,
-#'                             tracks= "/groups/stark/vloubiere/projects/gw_STARRSeq_bernardo/db/bw/DSCP_600bp_gw_cut_merged.bw", 
-#'                             col= c("lightgrey", "red"), 
-#'                             names = c("STARR-Seq"), 
-#'                             plot=F)
-#' vl_average_bw_track_plot_only(obj = obj1)
+#' obj1 <- vl_average_track(bed= STARR,
+#'                          set_IDs = c(rep("High", 2000), rep("Low", 2781)),
+#'                          #' extend = c(-5000, 5000),
+#'                          stranded = F,
+#'                          tracks= "/groups/stark/vloubiere/projects/gw_STARRSeq_bernardo/db/bw/DSCP_600bp_gw_cut_merged.bw", 
+#'                          col= c("lightgrey", "red"), 
+#'                          names = c("STARR-Seq"), 
+#'                          plot=F)
+#' vl_average_track_plot_only(obj = obj1)
 #'
 #' Stranded 
 #' prom <- readRDS("/groups/stark/hendy/Projects/Chromatin_remodeler_specificity/Analyses/Rdata/unique_proms_merged.df.RDS")
 #' prom <- as.data.table(GRanges(unique(prom$Flybase.TSS)))
-#' obj2 <- vl_average_bw_track(bed= prom,
-#'                             extend = c(-5000, 5000),
-#'                             stranded = T,
-#'                             tracks= "/groups/stark/vloubiere/projects/available_data_dm3/db/bw/GSE81795_H3K4me3_rep1_uniq.bw",
-#'                             plot= F)
-#' vl_average_bw_track_plot_only(obj = obj2)
+#' obj2 <- vl_average_track(bed= prom,
+#'                          extend = c(-5000, 5000),
+#'                          stranded = T,
+#'                          tracks= "/groups/stark/vloubiere/projects/available_data_dm3/db/bw/GSE81795_H3K4me3_rep1_uniq.bw",
+#'                          plot= F)
+#' vl_average_track_plot_only(obj = obj2)
 #' @export
-vl_average_bw_track_plot_only <- function(obj, 
-                                          ylim= NULL,
-                                          xlab= "genomic distance",
-                                          ylab= "Enrichment",
-                                          axes= T, 
-                                          legend= T)
+vl_average_track_plot_only <- function(obj, 
+                                       ylim= NULL,
+                                       xlab= "genomic distance",
+                                       ylab= "Enrichment",
+                                       axes= T, 
+                                       legend= T)
 {
   if(is.null(ylim))
     ylim <- range(c(obj$heatmap[, mean-se], obj$heatmap[, mean+se]), na.rm = T)
