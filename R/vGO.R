91d0e1ce9ad8c3cc48dd2b44319293b0ca1114eb
Vincent Loubiere <vincent.loubiere@hotmail.fr>
2021-06-03 10:55:23 +0200

    vGO function single vector

--- R/vGO.R
+++ R/vGO.R
@@ -1,84 +1,107 @@
-# require(data.table)
-# require(ontologyIndex)
-# 
-# test <- readRDS("Rdata/final_clustering_transcriptomes.rds")
-# test <- lapply(split(test, by= "cl"), function(x) x$FBgn)
-# 
-# vl_GO_cluster <- function(dat_list, 
-#                           type,
-#                           plot= T,
-#                           padj_cutoff= 0.001,
-#                           log2OR_cutoff_FUN= function(x) x>1,
-#                           
-#                           go_object= )
-# {
-#   if(!is.list(dat_list))
-#     stop("dat_list must be a list object") else
-#       dat <- rbindlist(lapply(dat_list, as.data.table), idcol = "cl")
-#   if(!type %in% c("FBgn", "Symbol"))
-#     stop("Type must be either FBgn or Symbol")
-#   dat[, cl:= as.factor(cl)]
-#   colnames(dat)[2] <- type
-#   
-#   # Compute enrichment
-#   go <- copy(go_object)
-#   go[, genes_GO:= length(unique(Fbgn)), GO]
-#   go[, genes_noGO:= length(unique(go$Fbgn))-genes_GO]
-#   enrich <- dat[, {
-#     .c <- unique(FBgn)
-#     res <- go[.c, .(genes_GO, 
-#                     genes_noGO, 
-#                     cl_GO= length(unique(Fbgn))), 
-#               on= "Fbgn", 
-#               by= .(GO, name), 
-#               nomatch= NULL]
-#     res[, cl_noGO:= length(.c)-cl_GO]
-#     res <- unique(res)
-#     res[, fisher.test(data.table(c(cl_GO, cl_noGO), 
-#                                  c(genes_GO, genes_noGO)))[c("estimate", "p.value")], (res)]
-#   }, cl]
-#   enrich[, p.adj:= p.adjust(p.value, method = "fdr"), cl]
-#   enrich[, log2OR:= log2(estimate)]
-#   
-#   # Select signif ontologies
-#   sel <- enrich[, any(p.adj<=padj_cutoff & log2OR_cutoff_FUN(log2OR)), GO][(V1), GO]
-#   dmat <- enrich[GO %in% sel]
-#   setorderv(dmat, c("cl", "log2OR"), c(1, -1))
-#   dmat[, x:= .GRP, .(cl, log2OR)]
-#   dmat[, x:= min(.SD[p.adj<=padj_cutoff, x]), name]
-#   dmat[, x:= .GRP, keyby= x]
-#   setorderv(dmat, "cl", order = -1)
-#   dmat[, y:= .GRP, cl]
-#   
-#   # PLOT
-#   Cc <- cut(dmat$p.adj, 
-#             c(-Inf, 0.00001, 0.001, 0.01, 0.05, Inf), 
-#             c("red", "tomato", "cornflowerblue", "blue", "black"))
-#   
-#   
-#   plot.new()
-#   .v <- seq(0, 1, length.out = max(dmat$x))
-#   segments(.v, 0, .v, 1, lwd= 0.5)
-#   .h <- seq(0, 1, length.out = max(dmat$y))
-#   segments(0, .h, 1, .h, lwd= 0.5)
-# 
-#   points(x= .v[dmat$x], 
-#          y= .h[dmat$y], 
-#          cex= dmat$log2OR/2.5, 
-#          col= as.character(Cc), 
-#          pch= 16, 
-#          xpd= T)
-#   axis(2, 
-#        at = .h, 
-#        labels = unique(dmat$cl), 
-#        las= 1, 
-#        tick= 0, 
-#        lwd= 0)
-#   par(cex= 0.7)
-#   axis(1, 
-#        at = .v, 
-#        labels = unique(dmat[, .(name, x)])$name, 
-#        las= 2, 
-#        tick= 0, 
-#        lwd= 0)
-# }
+#' GO analysis 
+#'
+#' This function compute GO enrichment in a group of genes
+#'
+#' @param FBgn_list A vector of FBgn IDs
+#' @param go_object object containing GO data. see ?vl_fb_go_table_dm6_FB2020_05
+#' @param FBgn_universe Vector of FBgn IDs to which the analysis will be restricted. default= "all" (i.e. all the FBGns present in the table)
+#' @param main title
+#' @export
+
+vl_GO_enrich <- function(FBgn_vector,
+                         go_object= vl_fb_go_table_dm6_FB2020_05,
+                         FBgn_universe= "all",
+                         main= "")
+{
+  # Checks
+  if(!is.vector(FBgn_vector))
+    stop(paste0("FBgn_vector should be a vector FBgn IDs"))
+  if(!all(grepl("FBgn", FBgn_vector)))
+    stop(paste0("Some FBgn_vector components are not FBgn IDs"))
+  if(length(FBgn_universe)==1)
+    if(FBgn_universe=="all")
+      FBgn_universe <- unique(go_object$FBgn)  
+    
+    # Restrict GO object to universe
+    go_current <- copy(go_object)[FBgn %in% FBgn_universe, .(GO, name, type= unlist(type), FBgn, Symbol)]
+    
+    # Compute go and total counts
+    go_current[, total_FBgn:= length(FBgn_universe)]
+    go_current[, total_go:= length(unique(FBgn)), GO]
+    
+    # Add cluster counts
+    go_current <- go_current[FBgn %in% FBgn_vector]
+    go_current[, total_cluster:= length(unique(FBgn))]
+    go_current[, go_cluster:= length(unique(FBgn)), GO]
+    
+    # Compute statistically enriched GOs
+    go_current <- go_current[, .(Symbol= .(Symbol), FBgn= .(FBgn)), 
+                             .(GO, name, type, total_go, total_FBgn, total_cluster, go_cluster)]
+    go_current[, c("estimate", "pvalue"):= {
+      fisher.test(matrix(c(go_cluster, # go+ cluster +
+                           total_cluster-go_cluster, # go- cluster +
+                           total_go-go_cluster, # go+ cluster -
+                           total_FBgn-total_cluster-total_go+go_cluster), # go- cluster -
+                         nrow=2, 
+                         byrow = T))[c("estimate", "p.value")]
+    }, total_go:go_cluster]
+    go_current[, '-log10(padj)':= -log10(p.adjust(pvalue, method = "fdr"))]
+    go_current[, log2OR:= log2(estimate)]
+    
+    # Generate plot table
+    pl <- go_current[`-log10(padj)`>5 & log2OR>0]
+    Cc <- colorRamp2(range(pl$log2OR), 
+                     colors = c("blue", "red"))
+    pl[, col:= Cc(log2OR)]
+    setorderv(pl, "-log10(padj)")
+    
+    # PLOT
+    par(mai= c(1, 
+               max(strwidth(pl$name, "inches")), 
+               0.5, 
+               1))
+    barplot(pl$`-log10(padj)`, 
+            col= pl$col, 
+            horiz = T,
+            xlab= "-log10(padj)", 
+            names.arg = pl$name,
+            las= 1, 
+            main= main, 
+            space = 0,
+            border= "white")
+    rasterImage(matrix(Cc(seq(min(pl$log2OR), 
+                              max(pl$log2OR), 
+                              length.out = 100)), 
+                       ncol= 1), 
+                xleft = grconvertX(1.075, "npc", "user"),
+                ybottom = grconvertY(0.93, "npc", "user"),
+                xright = grconvertX(1.125, "npc", "user"),
+                ytop = grconvertY(0.75, "npc", "user"), 
+                xpd= T)
+    text(grconvertX(1.075, "npc", "user"),
+         grconvertY(0.955, "npc", "user"), 
+         pos= 4,
+         "log2(OR)", 
+         xpd= T, 
+         offset= 0, 
+         cex= 0.8)
+    ticks <- axisTicks(range(pl$log2OR), log= F)
+    segments(grconvertX(1.125+0.005, "npc", "user"), 
+             grconvertY(0.75+ticks/max(pl$log2OR)*(0.93-0.75), "npc", "user"), 
+             grconvertX(1.125+0.01, "npc", "user"),
+             grconvertY(0.75+ticks/max(pl$log2OR)*(0.93-0.75), "npc", "user"),
+             xpd= T, 
+             lend= 2)
+    text(grconvertX(1.125+0.01, "npc", "user"), 
+         grconvertY(0.75+ticks/max(pl$log2OR)*(0.93-0.75), "npc", "user"),
+         labels = ticks,
+         cex= 0.6,
+         offset= 0.1,
+         pos= 4,
+         xpd= T)
+    
+    # RETURN
+    invisible(list(data= go_current, 
+                   plot= pl, 
+                   legend_ticks= ticks))
+}
