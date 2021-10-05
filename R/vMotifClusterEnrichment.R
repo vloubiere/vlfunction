7f998cfbf3e3fa8c7e9b2dd0ebe1af50e67f6d1e
vloubiere <vincent.loubiere@imp.ac.at>
2021-10-05 21:26:45 +0200

    fix mistake motifClusterEnrichment

--- R/vMotifClusterEnrichment.R
+++ R/vMotifClusterEnrichment.R
@@ -23,30 +23,32 @@ vl_motif_cl_enrich <- function(obj,
     stop("cl_column does not exist in provided obj") else if(cl_column != "cl")
       names(DT)[names(DT)==cl_column] <- "cl"
   if(class(DT$cl) != class(bg))
-    stop("cluster column and bg arg should share the same class!")
+    stop("cluster column and bg are not from same class -- > coerce?")
   if(!all(c("motif", "motif_counts", "motif_name") %in% names(DT)))
     stop("Provided DT should contain c('motif', 'motif_counts', 'motif_name') columns. see ?vl_motif_counts() output!")
   if(anyNA(bg))
     stop("bg contains NAs. cl_column should not contain NAs OR they should not be used for comparisons!")
   DT <- DT[, .(motif, motif_name, motif_counts, cl)]
-  setkeyv(DT, c("motif", "cl"))
-  N_mot <- length(unique(DT$motif))
-    
+
+  #-----------------------#
   # Format result table
+  #-----------------------#
   cmb <- DT[, {
     data.table(V1= na.omit(unique(cl))) # make DT containing clusters to test
   }, .(mot= motif, motif_name)]
   
+  #-----------------------#
   # For each motif/cluster combination, compute association using fisher
+  #-----------------------#
   cmb[, c("OR", "pval"):= {
     # Extract motif from DT, restrict to regions from tested cluster OR bg, cast contingency table
-    .t <- table(DT[.(mot, c(V1, bg)), .(cl==V1, 
-                                        eval(parse(text= comp_expr)))])
+    .t <- table(DT[motif==mot & cl %in% c(V1, bg), .(cl==V1, 
+                                                     eval(parse(text= comp_expr)))])
     # If motif present in both tested cl and bg, do fisher test
     if(identical(dim(.t), as.integer(c(2,2))))
       res <- as.data.table(as.list(fisher.test(.t)[c("estimate", "p.value")])) else
         data.table(numeric(), numeric())
-  }, .(mot, V1)] 
+  }, .(mot, V1)]
   cmb <- na.omit(cmb)
   cmb[, padj:= p.adjust(pval, method = "fdr"), pval]
   cmb[, log2OR:= log2(OR)]
