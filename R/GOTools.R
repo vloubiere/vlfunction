c758debbee34bf91ca915dad38e985a9a4982249
vloubiere <vincent.loubiere@imp.ac.at>
2022-08-14 03:07:49 +0200

    simplified data

--- R/GOTools.R
+++ R/GOTools.R
@@ -2,47 +2,72 @@
 #'
 #' This function compute GO enrichment in a group of genes
 #'
-#' @param FBgn_list A vector of FBgn IDs
-#' @param go_object object containing GO data. see ?vl_Dmel_GO_FB2020_05
-#' @param FBgn_universe Vector of FBgn IDs to which the analysis will be restricted. default= "all" (i.e. all the FBGns present in the table)
+#' @param geneSet_IDs A vector of FBgn IDs
+#' @param geneUniverse_IDs Vector of FBgn IDs to which the analysis will be restricted. If NULL, uses all genes.
+#' @param species Chose between "Dm" and "Mm" 
 #' @param plot Plot result?
 #' @param padj_cutoff cutoff for plotting
 #' @param top_enrich Show only n top enriched motifs
 #' @examples
 #' RpL <- vl_genes_set[GO=="RpL_genes", FBgn]
+#' Hox <- vl_genes_set[GO=="HOX_genes", FBgn]
 #' par(mar= c(4,20,2,6))
-#' vl_GO_enrich(RpL)
+#' vl_GO_enrich(RpL, species= "Dm")
+#' vl_GO_enrich(Hox, species= "Dm")
+#' vl_GO_enrich(RpL, Hox, species= "Dm")
 #' @export
-vl_GO_enrich <- function(FBgn_vector,
-                         go_object= vl_Dmel_GO_FB2020_05,
-                         FBgn_universe= "all",
-                         go_type= "all",
+vl_GO_enrich <- function(geneSet_IDs,
+                         geneUniverse_IDs= NULL,
+                         species,
                          plot= T,
                          padj_cutoff= 0.05,
                          top_enrich= Inf)
 {
-  obj <- data.table::copy(go_object)
-  FBgn_vector <- unique(FBgn_vector)
-  # Filter DB with depending on Universe
-  if(FBgn_universe!="all")
-    obj <- obj[FBgn %in% FBgn_universe]
-  # Keeps only GOs with at least one match
-  obj <- obj[GO %chin% obj[FBgn_vector, on= "FBgn"][,GO]]
-  # Counts genes per GO
-  all_genes <- unique(obj$FBgn)
-  check <- all_genes %chin% FBgn_vector
-  obj[, c("OR", "pval"):= {
-    tab <- table(all_genes %chin% FBgn,
-                 check)
+  db <- switch(species,
+               "Dm"= org.Dm.eg.db::org.Dm.eg.db,
+               "Mm"= org.Mm.eg.db::org.Mm.eg.db)
+  keyType <- switch(species, 
+                    "Dm"= "FLYBASE",
+                    "Mm"= "ENSEMBL")
+  # Extract geneSet GOs
+  set <- AnnotationDbi::select(x= db,
+                               keys = unique(c(geneSet_IDs)),
+                               keytype= keyType, 
+                               columns= "GOALL")
+  set <- data.table::as.data.table(set)
+  setnames(set, 
+           c(keyType, "GOALL"), 
+           c("ID", "GO"))
+  set <- unique(set[, .(ID, GO)])
+  # Extract GOs genes and restrict to universe
+  uni <- AnnotationDbi::select(x= db,
+                               keys = unique(set$GO),
+                               keytype= "GOALL", 
+                               columns= keyType)
+  uni <- data.table::as.data.table(uni)
+  setnames(uni, 
+           c(keyType, "GOALL"),
+           c("ID", "GO"))
+  if(!is.null(geneUniverse_IDs))
+    uni <- uni[set$ID, on= "ID"]
+  # Format and compute
+  DT <- rbind(SJ(set[, .(GO, ID)], set= T),
+              SJ(uni[, .(GO, ID)], set= F))
+  DT <- dcast(DT, set+ID~GO, fun.aggregate = length)
+  DT <- melt(DT, id.vars = c("set", "ID"), variable.name = "GO")
+  res <- DT[, {
+    tab <- table(factor(set, c(T,F)),
+                 factor(value>0, c(T,F)))
     fisher.test(tab, alternative = "greater")[c("estimate", "p.value")]
   }, GO]
-  
-  # padj...
-  res <- obj[, .(GO, 
-                 variable= name,
-                 log2OR= log2(OR),
-                 padj= p.adjust(pval, method = "fdr"))]
-  res <- unique(res)
+  res[, variable:= AnnotationDbi::select(GO.db,
+                                         keys = as.character(GO),
+                                         keytype= "GOID", 
+                                         columns= "TERM")$TERM]
+  # padj and export
+  res[, c("log2OR", "padj"):= .(log2(estimate), p.adjust(p.value, method = "fdr"))]
+  res$estimate <- NULL
+  res$p.value <- NULL
   setorderv(res, "log2OR")
   setattr(res, "class", c("vl_enr", "data.table", "data.frame"))
 
@@ -61,10 +86,9 @@ vl_GO_enrich <- function(FBgn_vector,
 #'
 #' This function compares GO enrichments for several groups
 #'
-#' @param FBgn_list A nameds list of FBgn IDs (1 sublist/cluster)
-#' @param go_object object containing GO data. see ?vl_fb_go_table_dm6_FB2020_05
-#' @param all_FBgns Vector of FBgns to be used in the universe, typically all the genes tested with DESeq. Default= "all" FBgns present in go_object.
-#' @param go_type The type of go to be considered. Default "all" means "biological_process" AND "cellular_component" AND "molecular_function". 
+#' @param FBgn_list A named list of FBgn IDs (1 sublist/cluster)
+#' @param geneUniverse_IDs Vector of FBgns to be used in the universe, typically all the genes tested with DESeq. If NULL, uses all genes.
+#' @param species Chose between "Dm" and "Mm" 
 #' @param plot Should the result be plot using balloons plot?
 #' @param padj_cutoff cutoff for ballons to be ploted
 #' @param log2OR_cutoff cutoff for ballons to be ploted
@@ -78,14 +102,13 @@ vl_GO_enrich <- function(FBgn_vector,
 #' @examples
 #' FBgn_list <- split(vl_genes_set$FBgn, vl_genes_set$GO)
 #' par(mar= c(5,25,2,5), las= 1)
-#' vl_GO_clusters(FBgn_list, auto_margins = F, cex.balloons = 0.3)
+#' vl_GO_clusters(FBgn_list, species= "Dm", auto_margins = F, cex.balloons = 0.3, padj_cutoff= 1e-5, top_enrich = 20)
 #' @export
 vl_GO_clusters <- function(FBgn_list,
-                           go_object= vl_Dmel_GO_FB2020_05,
-                           FBgn_universe= "all",
-                           go_type= "all",
+                           geneUniverse_IDs= NULL,
+                           species,
                            plot= T,
-                           padj_cutoff= 0.00001,
+                           padj_cutoff= 0.05,
                            log2OR_cutoff= 0,
                            top_enrich= Inf,
                            x_breaks,
@@ -95,19 +118,11 @@ vl_GO_clusters <- function(FBgn_list,
                            main= NA,
                            auto_margins = T)
 {
-  obj <- data.table::copy(go_object)
-  # Filter DB with depending on Universe
-  if(FBgn_universe!="all")
-    obj <- obj[FBgn %in% FBgn_universe]
-  if(is.null(names(FBgn_list)))
-    names(FBgn_list) <- paste0("cl", seq(FBgn_list))
-  
   # Compute enrichment per cluster
   obj <- lapply(FBgn_list, function(x) {
     vl_GO_enrich(x,
-                 go_object,
-                 FBgn_universe,
-                 go_type,
+                 geneUniverse_IDs,
+                 species= species,
                  plot= F)
   })
   names(obj) <- names(FBgn_list)
--- R/data.R
+++ R/data.R
@@ -17,41 +17,29 @@
 #' @source {"/groups/stark/almeida/data/motifs/motif_collection_v7_no_transfac_SteinAerts/TF_clusters_PWMs.RData"}
 "vl_Dmel_motifs_DB_full"
 
-#' Dm6 GO database 
+#' SUHW top peaks
 #'
-#' Contains GO annotation from FB2020_05
+#' Example set that can be used for motif enrichment...
 #'
-#' @usage Can be used with vl_GO_cluster. see ?vl_GO_cluster()
-#' 
-#' @format data.table containing GO metadata
-#' @examples
-#' GO: GO id
-#' name: GO full name
-#' type: GO type (biological_process, molecular_function, cellular_component)
-#' FBgn: gene ID
-#' Symbol: gene symbol
-#' 
-#' Object was generated using the following code:
-#' tmp <- tempfile(fileext = ".gz")
-#' download.file("http://ftp.flybase.net/releases/FB2020_05/precomputed_files/go/gene_association.fb.gz",
-#' destfile = tmp)
-#' go <- fread(tmp,
-#' skip= 5,
-#' select = c(2,3,5),
-#' col.names = c("FBgn", "Symbol", "GO"))
-#' tmp <- tempfile(fileext = ".gz")
-#' download.file("http://ftp.flybase.net/releases/FB2020_05/precomputed_files/ontologies/go-basic.obo.gz",
-#' destfile = tmp)
-#' details <- ontologyIndex::get_ontology(tmp, extract_tags = "everything")
-#' details <- data.table(GO= details$id,
-#' name= details$name,
-#' type= details$namespace)
-#' vl_Dmel_GO_FB2020_05 <- merge(details, unique(go))
-#' vl_Dmel_GO_FB2020_05 <- vl_Dmel_GO_FB2020_05[!(name %in% type)]
-#' vl_Dmel_GO_FB2020_05[, type:= unlist(type)]
-#' save(vl_Dmel_GO_FB2020_05,
-#' file= "/groups/stark/vloubiere/vlfunction/data/vl_Dmel_GO_FB2020_05.RData")
-"vl_Dmel_GO_FB2020_05"
+#' @usage see ?vl_motif_enrich()
+#' @format Narrowpeaks file containing top SUHW peaks on dm3 chanonical chromosomes
+"vl_SUHW_top_peaks"
+
+#' STARR-Seq top peaks
+#'
+#' Example set that can be used for motif enrichment...
+#'
+#' @usage see ?vl_motif_enrich()
+#' @format Narrowpeaks file containing top STARR-Seq peaks (DSCP) on dm3
+"vl_STARR_DSCP_top_peaks"
+
+#' Set of genes
+#'
+#' Example set that can be used for GO enrichment. Contain RpL and HOX genes
+#'
+#' @usage see ?vl_GO_enrich()
+#' @format FBgn, symbol and GO (RpL/HOX)
+"vl_genes_set"
 
 #' Thermofisher enzymes
 #'
@@ -94,81 +82,3 @@
 #'   paste0(.c, collapse= "")}, cutsite]
 #' @source {"https://www.thermofisher.com/at/en/home/brands/thermo-scientific/molecular-biology/thermo-scientific-restriction-modifying-enzymes/restriction-enzymes-thermo-scientific/conventional-restriction-enzymes-thermo-scientific.html"}
 "vl_thermofisher_restriction_enzymes_table"
-
-#' SUHW top peaks
-#'
-#' Example set that can be used for motif enrichment...
-#'
-#' @usage see ?vl_motif_enrich()
-#' @format Narrowpeaks file containing top SUHW peaks on dm3 chanonical chromosomes
-"vl_SUHW_top_peaks"
-
-#' STARR-Seq top peaks
-#'
-#' Example set that can be used for motif enrichment...
-#'
-#' @usage see ?vl_motif_enrich()
-#' @format Narrowpeaks file containing top STARR-Seq peaks (DSCP) on dm3
-"vl_STARR_DSCP_top_peaks"
-
-#' Set of genes
-#'
-#' Example set that can be used for GO enrichment. Contain RpL and HOX genes
-#'
-#' @usage see ?vl_GO_enrich()
-#' @format FBgn, symbol and GO (RpL/HOX)
-"vl_genes_set"
-
-#' Dm6 STRING database 
-#'
-#' Contains STRING annotations v11.5
-#'
-#' @usage Can be used with ?vl_STRING_interaction()
-#' @examples
-#' Code used to create DB
-#' # Download STRING db v11.5 from Dmel
-#' link <- "https://stringdb-static.org/download/protein.links.detailed.v11.5/7227.protein.links.detailed.v11.5.txt.gz"
-#' tmp <- tempfile(fileext = ".txt.gz")
-#' download.file(link, tmp)
-#' DB <- fread(tmp)
-#' # Download correspondance Dmel symbols
-#' link <- "https://stringdb-static.org/download/protein.info.v11.5/7227.protein.info.v11.5.txt.gz"
-#' download.file(link, tmp)
-#' sym <- fread(tmp)
-#' DB[sym, protein1_symbol:= i.preferred_name, on= "protein1==`#string_protein_id`"]
-#' DB[sym, protein2_symbol:= i.preferred_name, on= "protein2==`#string_protein_id`"]
-#' # Collapse unique interactions
-#' DB[, merge:= paste0(sort(c(protein1_symbol, protein2_symbol)), collapse= ""), .(protein1_symbol, protein2_symbol)]
-#' vl_Dmel_STRING_DB <- DB[, .SD[1], merge]
-#' # SAVE
-#' save(vl_Dmel_STRING_DB,
-#' file = "/groups/stark/vloubiere/vlfunction/data/vl_Dmel_STRING_DB.RData", 
-#' compress = "bzip2")
-"vl_Dmel_STRING_DB"
-
-#' mm10 STRING database 
-#'
-#' Contains STRING annotations v11.5
-#'
-#' @usage Can be used with ?vl_STRING_interaction()
-#' @examples
-#' Code used to create DB
-#' # Download STRING db v11.5 from Dmel
-#' link <- "https://stringdb-static.org/download/protein.links.detailed.v11.5/10090.protein.links.detailed.v11.5.txt.gz"
-#' tmp <- tempfile(fileext = ".txt.gz")
-#' download.file(link, tmp)
-#' DB <- fread(tmp)
-#' # Download correspondance Dmel symbols
-#' link <- "https://stringdb-static.org/download/protein.info.v11.5/10090.protein.info.v11.5.txt.gz"
-#' download.file(link, tmp)
-#' sym <- fread(tmp)
-#' DB[sym, protein1_symbol:= i.preferred_name, on= "protein1==`#string_protein_id`"]
-#' DB[sym, protein2_symbol:= i.preferred_name, on= "protein2==`#string_protein_id`"]
-#' # Collapse unique interactions
-#' DB[, merge:= paste0(sort(c(protein1_symbol, protein2_symbol)), collapse= ""), .(protein1_symbol, protein2_symbol)]
-#' vl_mm_STRING_DB <- DB[, .SD[1], merge]
-#' # SAVE
-#' save(vl_mm_STRING_DB,
-#' file = "/groups/stark/vloubiere/vlfunction/data/vl_mm_STRING_DB.RData", 
-#' compress = "bzip2")
-"vl_mm_STRING_DB"
