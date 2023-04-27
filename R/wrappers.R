c92a8ab04be4e40fbedf929706e5af65a36409c8
vloubiere <vincent.loubiere@imp.ac.at>
2023-04-27 09:28:41 +0200

    fix var dropbox download

--- R/wrappers.R
+++ R/wrappers.R
@@ -63,7 +63,7 @@ vl_dropbox_upload <- function(local_path, remote_path)
 #' @export
 vl_dropbox_download <- function(local_path, remote_path)
 {
-  cmd <- paste("sh  /groups/stark/vloubiere/apps/dropbox/dropbox_uploader.sh download", local_path, remote_path)
+  cmd <- paste("sh  /groups/stark/vloubiere/apps/dropbox/dropbox_uploader.sh download", remote_path, local_path)
   system(cmd)
 }
 
