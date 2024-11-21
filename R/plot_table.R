44c97cdc358b492161da3ffc5765ed2f9816501e
vloubiere <vincent.loubiere@imp.ac.at>
2024-11-21 08:32:43 +0100

    add color args to plot_table

--- R/plot_table.R
+++ R/plot_table.R
@@ -3,11 +3,17 @@
 #' Plots a data.table as text
 #'
 #' @param DT data.table
+#' @param cex Expansion factor for the table
 #' @param wrap.text Number of characters after which text can be wrapped
+#' @param header.col Color for header row
+#' @param row.cols Colors for non-header rows
 #' @return Plots table
 #' @export
 vl_plot_table <- function(DT,
-                          wrap.text= 20)
+                          cex= 1,
+                          wrap.text= 20,
+                          header.col= "grey50",
+                          row.cols= c("grey70", "grey90"))
 {
   frame <- as.data.table(as.list(names(DT)))
   setnames(frame, names(DT))
@@ -18,18 +24,18 @@ vl_plot_table <- function(DT,
   frame[, value:= gsub("\n$", "", value)]
   
   plot.new()
-  frame[, width:= max(strwidth(value))+strwidth("M")*2, variable]
-  frame[, height:= max(strheight(value)+strheight("M")), row]
+  frame[, width:= max(strwidth(value, cex= cex))+strwidth("M", cex= cex)*2, variable]
+  frame[, height:= max(strheight(value, cex= cex)+strheight("M", cex= cex)), row]
   xleft <- unique(frame[, .(variable, width)])
   xleft[, xleft:=  0.5-(sum(width)/2)+cumsum(data.table::shift(width, fill = 0))]
   frame[xleft, xleft:= xleft, on= "variable"]
   top <- unique(frame[, .(row, height)])
   top[, top:= 0.5+(sum(height)/2)-cumsum(data.table::shift(height, fill = 0))]
   frame[top, top:= top, on= "row"]
-  frame[, col:= rep_len(c("grey70", "grey90"), .N), xleft]
-  frame[row==1, col:= "grey50"]
+  frame[row>1, col:= rep_len(row.cols, .N), xleft]
+  frame[row==1, col:= header.col]
   
   frame[, rect(xleft, top-height, xleft+width, top, xpd= T, col= col, border= "white")]
-  frame[, text(xleft+width/2, top-height/2, value, xpd= T)]
+  frame[, text(xleft+width/2, top-height/2, value, xpd= T, cex= cex)]
   invisible(frame)
 }
